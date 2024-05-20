72d4883f70624de62c5b16ba1a87ff48681fafe8
Prasanna B <122286297+Prasannnnn@users.noreply.github.com>
2024-05-20 18:18:28 +0530

    Portfolio pages

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('theme-toggle-btn').addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+});
